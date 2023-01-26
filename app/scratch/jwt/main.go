0e0110c3a2eb7396670c60c3caca9a7a7033e59b
William Kennedy <bill@ardanlabs.com>
2023-01-26 15:22:52 -0500

    commit 47

--- app/scratch/jwt/main.go
+++ app/scratch/jwt/main.go
@@ -8,6 +8,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func main() {
@@ -25,11 +28,11 @@ func run() error {
 	}
 
 	// Create a file for the private key information in PEM form.
-	privateFile, err := os.Create("private.pem")
-	if err != nil {
-		return fmt.Errorf("creating private file: %w", err)
-	}
-	defer privateFile.Close()
+	// privateFile, err := os.Create("private.pem")
+	// if err != nil {
+	// 	return fmt.Errorf("creating private file: %w", err)
+	// }
+	// defer privateFile.Close()
 
 	// Construct a PEM block for the private key.
 	// privateBlock := pem.Block{
@@ -41,8 +44,6 @@ func run() error {
 	// if err := pem.Encode(privateFile, &privateBlock); err != nil {
 	// 	return fmt.Errorf("encoding to private file: %w", err)
 	// }
-
-	// =========================================================================
 
 	// Marshal the public key from the private key to PKIX.
 	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
@@ -62,6 +63,46 @@ func run() error {
 	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
 		return fmt.Errorf("encoding to public file: %w", err)
 	}
+
+	// =========================================================================
+
+	fmt.Print("\n========================================\n\n")
+
+	// Generating a token requires defining a set of claims. In this applications
+	// case, we only care about defining the subject and the user in question and
+	// the roles they have on the database. This token will expire in a year.
+	//
+	// iss (issuer): Issuer of the JWT
+	// sub (subject): Subject of the JWT (the user)
+	// aud (audience): Recipient for which the JWT is intended
+	// exp (expiration time): Time after which the JWT expires
+	// nbf (not before time): Time before which the JWT must not be accepted for processing
+	// iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+	// jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only once)
+	claims := struct {
+		jwt.RegisteredClaims
+		Roles []string
+	}{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   "123456789",
+			Issuer:    "service project",
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(8760 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("RS256"), claims)
+	token.Header["kid"] = "kid1"
+
+	str, err := token.SignedString(privateKey)
+	if err != nil {
+		return fmt.Errorf("signing token: %w", err)
+	}
+
+	fmt.Println(str)
+
+	// =========================================================================
 
 	fmt.Print("\n========================================\n\n")
 	return nil

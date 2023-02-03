b0999c7223ff2a9e6f0262cb721f6f880c760d75
William Kennedy <bill@ardanlabs.com>
2023-02-03 13:33:15 -0500

    commit 67

--- business/core/user/user.go
+++ business/core/user/user.go
@@ -8,9 +8,11 @@ import (
 	"errors"
 	"fmt"
 	"net/mail"
+	"time"
 
 	"github.com/ardanlabs/service/business/sys/validate"
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Set of error variables for CRUD operations.
@@ -52,4 +54,108 @@ func (c *Core) Create(ctx context.Context, nu NewUser) (User, error) {
 		return User{}, fmt.Errorf("validating data: %w", err)
 	}
 
+	hash, err := bcrypt.GenerateFromPassword([]byte(nu.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return User{}, fmt.Errorf("generating password hash: %w", err)
+	}
+
+	now := time.Now()
+
+	usr := User{
+		ID:           uuid.New(),
+		Name:         nu.Name,
+		Email:        nu.Email,
+		PasswordHash: hash,
+		Roles:        nu.Roles,
+		Enabled:      true,
+		DateCreated:  now,
+		DateUpdated:  now,
+	}
+
+	// This provides an example of how to execute a transaction if required.
+	tran := func(s Storer) error {
+		if err := s.Create(ctx, usr); err != nil {
+			return fmt.Errorf("create: %w", err)
+		}
+		return nil
+	}
+
+	if err := c.storer.WithinTran(ctx, tran); err != nil {
+		return User{}, fmt.Errorf("tran: %w", err)
+	}
+
+	return usr, nil
+}
+
+// Update replaces a user document in the database.
+func (c *Core) Update(ctx context.Context, usr User, uu UpdateUser) (User, error) {
+	if err := validate.Check(uu); err != nil {
+		return User{}, fmt.Errorf("validating data: %w", err)
+	}
+
+	if uu.Name != nil {
+		usr.Name = *uu.Name
+	}
+	if uu.Email != nil {
+		usr.Email = *uu.Email
+	}
+	if uu.Roles != nil {
+		usr.Roles = uu.Roles
+	}
+	if uu.Password != nil {
+		pw, err := bcrypt.GenerateFromPassword([]byte(*uu.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return User{}, fmt.Errorf("generating password hash: %w", err)
+		}
+		usr.PasswordHash = pw
+	}
+	if uu.Enabled != nil {
+		usr.Enabled = *uu.Enabled
+	}
+	usr.DateUpdated = time.Now()
+
+	if err := c.storer.Update(ctx, usr); err != nil {
+		return User{}, fmt.Errorf("update: %w", err)
+	}
+
+	return usr, nil
+}
+
+// Delete removes a user from the database.
+func (c *Core) Delete(ctx context.Context, usr User) error {
+	if err := c.storer.Delete(ctx, usr); err != nil {
+		return fmt.Errorf("delete: %w", err)
+	}
+
+	return nil
+}
+
+// Query retrieves a list of existing users from the database.
+func (c *Core) Query(ctx context.Context, pageNumber int, rowsPerPage int) ([]User, error) {
+	users, err := c.storer.Query(ctx, pageNumber, rowsPerPage)
+	if err != nil {
+		return nil, fmt.Errorf("query: %w", err)
+	}
+
+	return users, nil
+}
+
+// QueryByID gets the specified user from the database.
+func (c *Core) QueryByID(ctx context.Context, userID uuid.UUID) (User, error) {
+	user, err := c.storer.QueryByID(ctx, userID)
+	if err != nil {
+		return User{}, fmt.Errorf("query: %w", err)
+	}
+
+	return user, nil
+}
+
+// QueryByEmail gets the specified user from the database by email.
+func (c *Core) QueryByEmail(ctx context.Context, email mail.Address) (User, error) {
+	user, err := c.storer.QueryByEmail(ctx, email)
+	if err != nil {
+		return User{}, fmt.Errorf("query: %w", err)
+	}
+
+	return user, nil
 }
--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/open-policy-agent/opa v0.48.0
 	go.uber.org/automaxprocs v1.5.1
 	go.uber.org/zap v1.24.0
+	golang.org/x/crypto v0.5.0
 )
 
 require (
@@ -32,7 +33,6 @@ require (
 	github.com/yashtewari/glob-intersection v0.1.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/crypto v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect

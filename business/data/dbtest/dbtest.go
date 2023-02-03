ba9f65b86aaa5982589417ffb76eb95a3d3c87ed
William Kennedy <bill@ardanlabs.com>
2023-02-03 14:35:13 -0500

    commit 70

---
+++ business/data/dbtest/dbtest.go
@@ -0,0 +1,131 @@
+// Package dbtest contains supporting code for running tests that hit the DB.
+package dbtest
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/ardanlabs/service/business/data/dbschema"
+	"github.com/ardanlabs/service/business/sys/database"
+	"github.com/ardanlabs/service/foundation/docker"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// Success and failure markers.
+const (
+	Success = "\u2713"
+	Failed  = "\u2717"
+)
+
+// StartDB starts a database instance.
+func StartDB() (*docker.Container, error) {
+	image := "postgres:15-alpine"
+	port := "5432"
+	args := []string{"-e", "POSTGRES_PASSWORD=postgres"}
+
+	c, err := docker.StartContainer(image, port, args...)
+	if err != nil {
+		return nil, fmt.Errorf("starting container: %w", err)
+	}
+
+	fmt.Printf("Image:       %s\n", image)
+	fmt.Printf("ContainerID: %s\n", c.ID)
+	fmt.Printf("Host:        %s\n", c.Host)
+
+	return c, nil
+}
+
+// StopDB stops a running database instance.
+func StopDB(c *docker.Container) {
+	docker.StopContainer(c.ID)
+	fmt.Println("Stopped:", c.ID)
+}
+
+// NewUnit creates a test database inside a Docker container. It creates the
+// required table structure but the database is otherwise empty. It returns
+// the database to use as well as a function to call at the end of the test.
+func NewUnit(t *testing.T, c *docker.Container, dbName string) (*zap.SugaredLogger, *sqlx.DB, func()) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	dbM, err := database.Open(database.Config{
+		User:       "postgres",
+		Password:   "postgres",
+		Host:       c.Host,
+		Name:       "postgres",
+		DisableTLS: true,
+	})
+	if err != nil {
+		t.Fatalf("Opening database connection: %v", err)
+	}
+
+	t.Log("Waiting for database to be ready ...")
+
+	if err := database.StatusCheck(ctx, dbM); err != nil {
+		t.Fatalf("status check database: %v", err)
+	}
+
+	t.Log("Database ready")
+
+	if _, err := dbM.ExecContext(context.Background(), "CREATE DATABASE "+dbName); err != nil {
+		t.Fatalf("creating database %s: %v", dbName, err)
+	}
+	dbM.Close()
+
+	// =========================================================================
+
+	db, err := database.Open(database.Config{
+		User:       "postgres",
+		Password:   "postgres",
+		Host:       c.Host,
+		Name:       dbName,
+		DisableTLS: true,
+	})
+	if err != nil {
+		t.Fatalf("Opening database connection: %v", err)
+	}
+
+	t.Log("Migrate and seed database ...")
+
+	if err := dbschema.Migrate(ctx, db); err != nil {
+		t.Logf("Logs for %s\n%s:", c.ID, docker.DumpContainerLogs(c.ID))
+		t.Fatalf("Migrating error: %s", err)
+	}
+
+	if err := dbschema.Seed(ctx, db); err != nil {
+		t.Logf("Logs for %s\n%s:", c.ID, docker.DumpContainerLogs(c.ID))
+		t.Fatalf("Seeding error: %s", err)
+	}
+
+	t.Log("Ready for testing ...")
+
+	var buf bytes.Buffer
+	encoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+	writer := bufio.NewWriter(&buf)
+	log := zap.New(
+		zapcore.NewCore(encoder, zapcore.AddSync(writer), zapcore.DebugLevel),
+		zap.WithCaller(true),
+	).Sugar()
+
+	// teardown is the function that should be invoked when the caller is done
+	// with the database.
+	teardown := func() {
+		t.Helper()
+		db.Close()
+
+		log.Sync()
+
+		writer.Flush()
+		fmt.Println("******************** LOGS ********************")
+		fmt.Print(buf.String())
+		fmt.Println("******************** LOGS ********************")
+	}
+
+	return log, db, teardown
+}
